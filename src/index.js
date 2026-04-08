2a5a7045a357ca4cd3cf25b70128a72bdae3896b
Parthipan <parthipanmookkandi@gmail.com>
2026-04-08 13:46:13 +0530

    base name updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/malar_products">
     <App />
     </BrowserRouter>
   </React.StrictMode>
